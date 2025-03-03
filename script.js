c087857b05ed05b725320cd4fba6bf9ed241390a
fakersl <zgustovo13365@gmail.com>
2025-03-02 22:21:18 -0300

    Adequando o JS ao HTML

--- script.js
+++ script.js
@@ -4,41 +4,51 @@ let sizePassword = document.querySelector("#value_password");
 let password = document.querySelector("#random_password");
 
 let containerPassword = document.querySelector("#container_password");
+let tooltip = document.querySelector("#tooltip");
 
 let charset = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ@#*&_-!$"
 let newPassword = "";
 
+// Atualiza o tamanho da senha conforme o slider
 sizePassword.innerHTML = sliderElement.value;
 
-sliderElement.oninput = function(){
+sliderElement.oninput = function() {
     sizePassword.innerHTML = this.value;
 }
 
-//GERA A SENHA
-function generatePassword(){
+// Função para gerar a senha
+function generatePassword() {
     let pass = "";
-    for(let i = 0, n = charset.length; i < sliderElement.value; i++){
+    for (let i = 0, n = charset.length; i < sliderElement.value; i++) {
         pass += charset.charAt(Math.floor(Math.random() * n));
     }
 
     password.innerHTML = pass;
-    containerPassword.classList.remove("hide");
+    containerPassword.classList.remove("hidden");
     newPassword = pass;
 }
 
-// Copia a senha
-function copiarSenha() {
+// Função para copiar a senha
+function copyPassword() {
     navigator.clipboard.writeText(newPassword)
-      .then(() => {
-        alert("Senha copiada para a área de transferência!");
-      })
-      .catch(err => {
-        console.error("Erro ao copiar:", err);
-        alert("Cópia não suportada ou ocorreu um erro.");
-      });
-  }
-  
-  // Aciona a cópia ao clicar no botão
-  const copiarButton = document.querySelector("#container_password button");
-  copiarButton.addEventListener("click", copiarSenha);
-  
+        .then(() => {
+            // Exibe o tooltip de "Senha copiada!"
+            tooltip.textContent = "Senha copiada!";
+            tooltip.classList.add('opacity-100');
+            setTimeout(() => {
+                tooltip.classList.remove('opacity-100');
+                tooltip.classList.add('opacity-0');
+            }, 2000);
+        })
+        .catch(err => {
+            console.error("Erro ao copiar:", err);
+            alert("Cópia não suportada ou ocorreu um erro.");
+        });
+}
+
+// Aciona a cópia ao clicar na senha ou no botão
+const copyButton = document.querySelector("#copy_button");
+copyButton.addEventListener("click", copyPassword);
+
+// Aciona a cópia ao clicar na senha gerada
+password.addEventListener("click", copyPassword);
